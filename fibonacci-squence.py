cd54ee35eee7b9a4fd4fc804622519f35d309dfb
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-06-22 14:12:36 +0200

    Update fibonacci-squence.py

--- fibonacci-squence.py
+++ fibonacci-squence.py
@@ -2,19 +2,24 @@ try:
     import sys
     xv="01"
     inp=sys.argv[1]
-    x=0
-    y=1
-    def Fibo(x,y):
-        z = x + y
-        return x,y,z
-    def CountingFibo(x,y,xv):
-        for i in range(1,int(inp)):
-            x, y, z = Fibo(x,y)
-            x=y
-            y=z
-            xv = xv + str(z)
-        return xv, str(z)
-    aa,bb=CountingFibo(x,y,xv)
-    print(bb)
+    if inp == "0":
+        print("0")
+    elif inp == "1":
+        print("1")
+    else:
+        x=0
+        y=1
+        def Fibo(x,y):
+            z = x + y
+            return x,y,z
+        def CountingFibo(x,y,xv):
+            for i in range(1,int(inp)):
+                x, y, z = Fibo(x,y)
+                x=y
+                y=z
+                xv = xv + str(z)
+            return xv, str(z)
+        aa,bb=CountingFibo(x,y,xv)
+        print(bb)
 except:
     print("\n ¬ Usage  ;  fibancci.py <iteration>       exemple :: fibancci.py  14  will return  377\n")
