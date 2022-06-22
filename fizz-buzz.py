3d980940b80b325d9724cd63da9fc7dd5249765b
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-06-22 15:16:17 +0200

    Update fizz-buzz.py

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -2,10 +2,10 @@ import random
 for z in range(0, 50):
     x = random.randrange(0, 100)
     if x % 3 == 0 and x % 5 == 0:
-        print(x, " FizzBuzz")
+        print(x, "\t\tFizzBuzz")
     elif x % 3 == 0:
-        print(x," Fizz")
+        print(x,"\t\tFizz")
     elif x % 5 == 0:
-        print(x, " Buzz")
+        print(x, "\t\tBuzz")
     else:
         print(x)
