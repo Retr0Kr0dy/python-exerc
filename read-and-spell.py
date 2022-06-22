f67e39827f989e4767b7a63249d8d1e2ea62fe8f
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-06-22 15:09:03 +0200

    Update read-and-spell.py

--- read-and-spell.py
+++ read-and-spell.py
@@ -1,31 +1,39 @@
-xv = "1"
-def spell(xv):
-    xv = xv + "!"
-    Lxv = []
-    for xx in xv:
-        Lxv.append(xx)
-    Lresp = []
-    def reading():
-        Ltmp = []
-        while len(Lxv) >= 2:
-            av = Lxv[0]
-            if av == Lxv[1]:
-                Ltmp.append(Lxv[1])
-                Lxv.pop(1)
-            else:
-                yv = 0
-                yv = len(Ltmp) + 1
-                Lresp.append(yv)
-                try:
-                    Lresp.append(Lxv[0])
-                except:
-                    Lresp.append("1")
-                Lxv.pop(0)
-                Ltmp=[]
-    reading()
-    xv = ''.join(str(e) for e in Lresp)
-    print("")
-    print("Lresp = ", xv)
-    input("\n  ...  ...  ...  ...  ...  ...  ...")
+try:
+    import sys
+    try:
+        xv =sys.argv[1]
+    except:
+        xv = "1"
+    def spell(xv):
+        xv = xv + "!"
+        Lxv = []
+        for xx in xv:
+            Lxv.append(xx)
+        Lresp = []
+        def reading():
+            Ltmp = []
+            while len(Lxv) >= 2:
+                av = Lxv[0]
+                if av == Lxv[1]:
+                    Ltmp.append(Lxv[1])
+                    Lxv.pop(1)
+                else:
+                    yv = 0
+                    yv = len(Ltmp) + 1
+                    Lresp.append(yv)
+                    try:
+                        Lresp.append(Lxv[0])
+                    except:
+                        Lresp.append("1")
+                    Lxv.pop(0)
+                    Ltmp=[]
+        reading()
+        xv = ''.join(str(e) for e in Lresp)
+        print("")
+        input(xv)
+        spell(xv)
     spell(xv)
-spell(xv)
+except:
+    print("""\n\n\n  Usage  ;  read-and-spell.py (starting with 1) |or| read-and-spell.py <integer> (starting with int)
+    
+    press enter to continue printing result""")
